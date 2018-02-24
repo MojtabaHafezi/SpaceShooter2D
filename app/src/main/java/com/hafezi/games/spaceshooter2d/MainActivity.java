05a1184f19d6f8c9a932c2787dee065f4826193b
Mojtaba Hafezi <mojtaba.hafezi@hotmail.de>
2018-02-24 22:39:18 +0000

    ADD: Bluetooth permissions and connection

--- app/src/main/java/com/hafezi/games/spaceshooter2d/MainActivity.java
+++ app/src/main/java/com/hafezi/games/spaceshooter2d/MainActivity.java
@@ -1,11 +1,17 @@
 package com.hafezi.games.spaceshooter2d;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     Button highscoreButton;
     Button exitButton;
     private SoundManager soundManager;
+    //bluetooth
+    private BluetoothAdapter bluetoothAdapter;
+    private Set<BluetoothDevice> pairedDevices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,24 @@ public class MainActivity extends AppCompatActivity {
         optionButton = (Button) findViewById(R.id.optionButton);
         highscoreButton = (Button) findViewById(R.id.scoreButton);
         exitButton = (Button) findViewById(R.id.exitButton);
+
+        //activate bluetooth
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        //if device supports bluetooth -> activate if not already on
+        if (! (bluetoothAdapter == null)) {
+            if(!bluetoothAdapter.isEnabled())
+            {
+                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnOn, 0);
+            }
+            pairedDevices = bluetoothAdapter.getBondedDevices();
+            for(BluetoothDevice bluetoothDevice : pairedDevices)
+            {
+                Log.e("DEVICE:",  bluetoothDevice.getName());
+            }
+        }
+
+
 
         //Set the Listeners
         setButtonListeners();
